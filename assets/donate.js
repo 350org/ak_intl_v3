083cd91b56f3b8952afe35f55b96da74d33a3906
Sukhada Kulkarni <sukhada@withtheranks.com>
2022-08-18 08:40:18 -0700

    Fix Firefox bug

--- assets/donate.js
+++ assets/donate.js
@@ -143,6 +143,10 @@ function clear_radio_buttons() {
       }
     })
 
+    // Set Donation Type to "Donate Once" on page load
+    $('#donation_type').val('single'); 
+
+    // Toggle checkmark depending on Donation Type selection
     if ($('#donation_type').val("single")) {
       $('.once-checkmark').removeClass('v-hide');
       $('.monthly-checkmark').addClass('v-hide');
